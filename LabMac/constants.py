347550a134e73b1b484a4734eb554e23665ea7a4
Jody Etienne <etiennody@gmail.com>
2019-11-25 16:48:36 +0100

    add constants file

---
+++ LabMac/constants.py
@@ -0,0 +1,14 @@
+#!/usr/bien/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Project 3 - OpenClassrooms: Aidez MacGyver à s'échapper !
+Author:
+    Jody Etienne
+Github:
+    https://github.com/etiennody
+"""
+
+START_MAZE = "S"
+EXIT_MAZE = "E"
+NUMBER_ITEMS = 3
