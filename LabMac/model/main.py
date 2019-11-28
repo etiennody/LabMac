de1e05df223e513599bf643ccc223ed7824dd025
Jody Etienne <etiennody@gmail.com>
2019-11-28 17:32:17 +0100

    fix remove get_random_weapons

--- LabMac/model/main.py
+++ LabMac/model/main.py
@@ -6,8 +6,6 @@ import Maze.maze
 def main(self):
     maze = Maze()
     while True:
-        # Game initialisation
-        weapons = get_random_weapons(3)
         resp = input("where do you want to move (u, d, l r)?")
         if resp == "u":
             maze.move_hero_up()
