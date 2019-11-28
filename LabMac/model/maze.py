e65e26936fee31ee406b7c614e1cbe3febd88a22
Jody Etienne <etiennody@gmail.com>
2019-11-28 13:21:56 +0100

    add hero in maze

--- LabMac/model/maze.py
+++ LabMac/model/maze.py
@@ -10,7 +10,7 @@ Github:
 import random
 
 from Labmac import constants as char
-from position import Position
+from position import Position as pos
 from items import Floor, Hero
 
 
@@ -18,21 +18,22 @@ class Maze:
     def __init__(self, filename):
         self.floor = []
         self.wall = []
+        self.weapons = []
         self.hero = None
         self.guardian = None
         self.load(filename)
 
-    def load(self, filename):
-        with open(filename):
-            for x in xrange(0, 15):
-                for y in xrange(0, 15):
-                    if char == FLOOR_MAZE:
+    def load(self):
+        with open():
+            for x in range(0, 15):
+                for y in range(0, 15):
+                    if char == pos.FLOOR_CHAR:
                         self.floor.append(Floor(x, y))
-                    elif char == START_MAZE:
+                    elif char == pos.START_CHAR:
                         self.hero = Hero(x, y)
                         self.floor.append(Floor(x, y))
-                    elif char == EXIT_MAZE:
-                        self.guardian = Guardian(x, y)
+                    elif char == pos.EXIT_CHAR:
+                        self.guardian = pos.Guardian(x, y)
                         self.floor.append(Floor(x, y))
                     else:
                         return "It's a wall"
@@ -45,3 +46,8 @@ class Maze:
             if new_weapons not in weapons:
                 weapons.append(new_weapons)
         return weapons
+
+    def add_hero(self, hero):
+        self.hero = hero
+        self.position = self.start
+        self.hero.maze = self
