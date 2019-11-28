e6c811ec8e455dace948e3d0c0e481f6401bc950
Jody Etienne <etiennody@gmail.com>
2019-11-28 12:04:26 +0100

    update maze

--- LabMac/model/maze.py
+++ LabMac/model/maze.py
@@ -7,10 +7,11 @@ Author:
 Github:
     https://github.com/etiennody
 """
-import random, sys
+import random
 
-from items import Floor
-from LabMac import constants
+from Labmac import constants as char
+from position import Position
+from items import Floor, Hero
 
 
 class Maze:
@@ -21,25 +22,26 @@ class Maze:
         self.guardian = None
         self.load(filename)
 
-    def load(self):
-        for x in xrange(0, 15):
-            for y in xrange(0, 15):
-                if char == FLOOR_MAZE:
-                    self.floor.append(Floor(x, y))
-                elif char == START_MAZE:
-                    self.hero = Hero(x, y)
-                    self.floor.append(Floor(x, y))
-                elif char == EXIT_MAZE:
-                    self.guardian = Guardian(x, y)
-                    self.floor.append(Floor(x, y))
-                else:
-                    pass
+    def load(self, filename):
+        with open(filename):
+            for x in xrange(0, 15):
+                for y in xrange(0, 15):
+                    if char == FLOOR_MAZE:
+                        self.floor.append(Floor(x, y))
+                    elif char == START_MAZE:
+                        self.hero = Hero(x, y)
+                        self.floor.append(Floor(x, y))
+                    elif char == EXIT_MAZE:
+                        self.guardian = Guardian(x, y)
+                        self.floor.append(Floor(x, y))
+                    else:
+                        return "It's a wall"
 
     def get_random_weapons(self, num_weapons):
         # create a [x, y] list of weapons
         weapons = []
         while len(weapons) < num_weapons:
-            new_weapons = [random.randint(x, y)]
+            new_weapons = [random.randint(self.floor)]
             if new_weapons not in weapons:
                 weapons.append(new_weapons)
         return weapons
