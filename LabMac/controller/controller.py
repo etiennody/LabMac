72baa0dce0ef070629040bb68affb22febed0566
Jody Etienne <etiennody@gmail.com>
2019-11-30 00:59:21 +0100

    update page layout

--- LabMac/controller/controller.py
+++ LabMac/controller/controller.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Project 3 - OpenClassrooms: Aidez MacGyver à s'échapper !
+Author:
+    Jody Etienne
+Github:
+    https://github.com/etiennody
+"""
+
 from LabMac.model.maze import Maze
 
 
