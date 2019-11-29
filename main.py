9a6d098c2e3e84f74d48928590ed2351fae855cb
Jody Etienne <etiennody@gmail.com>
2019-11-29 13:54:43 +0100

    add new main

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Project 3 - OpenClassrooms: Aidez MacGyver à s'échapper !
+Author:
+    Jody Etienne
+Github:
+    https://github.com/etiennody
+"""
+from LabMac.controller.controller import main
+
+if __name__ == "__main__":
+    main()
