fb9b2ef399672bc32964c442e79c48cb13018e3b
Jody Etienne <etiennody@gmail.com>
2019-12-01 17:29:53 +0100

    add comments for all

--- LabMac/model/position.py
+++ LabMac/model/position.py
@@ -11,20 +11,21 @@ Github:
 
 
 class Position:
-
+    # x, y : initial positions
     def __init__(self, x, y):
         self.set_position(x, y)
 
     def __repr__(self):
         return f"Position({self.x}, {self.y})"
 
+    # x, y : positions
     def set_position(self, x, y):
         self.x = x
         self.y = y
 
 
 class MoveablePosition(Position):
-
+    # Possibles positions to Up, Down, Left and Right
     def __init__(self, mx, my):
         self.mx = mx
         self.my = my
