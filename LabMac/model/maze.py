3f7b000efe25d4119c2c113ef9b8db09ed3f8620
Jody Etienne <etiennody@gmail.com>
2019-12-03 16:16:52 +0100

    fix maze file

--- LabMac/model/maze.py
+++ LabMac/model/maze.py
@@ -12,7 +12,8 @@ Github:
 import random
 
 from LabMac.constants import FLOOR_CHAR, START_CHAR, EXIT_CHAR
-from .position import Position
+from LabMac.model.position import Position
+from LabMac.model.items import Hero, Weapon
 
 
 class Maze:
@@ -27,7 +28,8 @@ class Maze:
         self.width = None
         self.height = None
         self.load(filename)
-        # self.random_position_weapons()
+        self.random_positions_weapons()
+        print(self.weapons)
 
     def __contains__(self, position):
         return position in self.floor
@@ -42,6 +44,7 @@ class Maze:
                     if character == START_CHAR:
                         self.start = position
                         self.floor.append(position)
+                        self.hero = Hero(x=char, y=lin)
                     elif character == EXIT_CHAR:
                         self.exit = position
                         self.floor.append(position)
@@ -53,48 +56,43 @@ class Maze:
             self.width = char + 1
             self.height = lin + 1
 
-    def add_hero(self, hero):
-        self.hero = hero
-        self.hero = self.start
-        self.hero.maze = self
+    def can_move_to(self, x, y):
+        return Position(x, y) in self.floor
 
-    # def can_move_to(self):
-    #     return Position(x, y) in self.maze.floor
+    def move_hero_up(self):
+        if self.can_move_to(self.hero.x, self.hero.y - 1):
+            self.hero.up()
+            # for weapon in self.weapons:
+            #     if self.hero == weapon:
+            #         self.hero.pick_up_weapon()
 
-    # @can_move_to
-    # def move_hero_up(self):
-    #     if self.can_move_to(self.hero.x - 1, self.hero.y):
-    #         self.hero.up()
-    #         for weapon in self.weapons:
-    #             if self.hero == weapon:
-    #                 self.hero.pick_up_weapon()
+    def move_hero_down(self):
+        if self.can_move_to(self.hero.x, self.hero.y + 1):
+            self.hero.down()
+            # for weapon in self.weapons:
+            #     if self.hero == weapon:
+            #         self.hero.pick_up_weapon()
 
-    # @can_move_to
-    # def move_hero_down(self):
-    #     if self.can_move_to(self.hero.x + 1, self.hero.y):
-    #         self.maze.down()
-    #         for weapon in self.weapons:
-    #             if self.hero == weapon:
-    #                 self.hero.pick_up_weapon()
+    def move_hero_left(self):
+        if self.can_move_to(self.hero.x - 1, self.hero.y):
+            self.hero.left()
+            # for weapon in self.weapons:
+            #     if self.hero == weapon:
+            #         self.hero.pick_up_weapon()
 
-    # def move_hero_left(self):
-    #     if self.can_move_to(self.hero.x, self.hero.y - 1):
-    #         self.hero.left()
-    #         for weapon in self.weapons:
-    #             if self.hero == weapon:
-    #                 self.hero.pick_up_weapon()
-
-    # def move_hero_right(self):
-    #     if self.can_move_to(self.hero.x, self.hero.y + 1):
-    #         self.hero.right()
-    #         for weapon in self.weapons:
-    #             if self.hero == weapon:
-    #                 self.hero.pick_up_weapon()
+    def move_hero_right(self):
+        if self.can_move_to(self.hero.x + 1, self.hero.y):
+            self.hero.right()
+            # for weapon in self.weapons:
+            #     if self.hero == weapon:
+            #         self.hero.pick_up_weapon()
 
     # get random positions for weapons
     def random_positions_weapons(self):
-        self.weapons = random.sample(
+        positions = random.sample(
             set(self.floor) - {self.start, self.exit}, 3)
-        weapons = ["ether", "pipe", "needle"]
-        for position in weapons:
-            self.weapons.append(weapons(x=position.x, y=position.y, name=weapons.pop()))
+        weapon_names = ["ether", "pipe", "needle"]
+        for position in positions:
+            self.weapons.append(
+                Weapon(x=position.x, y=position.y, name=weapon_names.pop())
+            )
