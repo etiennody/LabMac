aef72e1766d597bf1d4b2aaaacfa1ca7e8a29a3a
Jody Etienne <etiennody@gmail.com>
2019-12-03 16:11:11 +0100

    fix module position

--- LabMac/model/position.py
+++ LabMac/model/position.py
@@ -13,7 +13,8 @@ Github:
 class Position:
     # x, y : initial positions
     def __init__(self, x, y):
-        self.position(x, y)
+        super().__init__()
+        self.set_position(x, y)
 
     def __repr__(self):
         return f"Position({self.x}, {self.y})"
@@ -23,31 +24,28 @@ class Position:
         return self.x == other.x and self.y == other.y
 
     def __hash__(self):
-        return hash(self.position)
+        return self.x * 1000 + self.y
 
     # Define the addition between self position and move position
     def __add__(self, movement):
         return Position(self.x + movement.mx, self.y + movement.my)
 
     # x, y : positions
-    def position(self, x, y):
+    def set_position(self, x, y):
         self.x = x
         self.y = y
 
 
-class Movement:
-    def __init__(self, mx, my):
-        self.mx = mx
-        self.my = my
+class Movement(Position):
 
     def up(self):
-        self.mx -= 1
+        self.y -= 1
 
     def down(self):
-        self.mx += 1
+        self.y += 1
 
     def left(self):
-        self.my -= 1
+        self.x -= 1
 
     def right(self):
-        self.my += 1
+        self.x += 1
