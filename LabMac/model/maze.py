126fd3546a55a040b9e650e69f690bbf5b692658
Jody Etienne <etiennody@gmail.com>
2019-12-03 02:14:59 +0100

    fix load and add method for add hero

--- LabMac/model/maze.py
+++ LabMac/model/maze.py
@@ -18,7 +18,7 @@ from .position import Position
 class Maze:
     def __init__(self, filename):
         self.floor = []
-        # self.walls = []
+        self.walls = []
         self.weapons = []
         self.hero = None
         # self.guardian = None
@@ -27,65 +27,74 @@ class Maze:
         self.width = None
         self.height = None
         self.load(filename)
-        self.random_position_weapons()
+        # self.random_position_weapons()
 
-    # def __contains__(self, position):
-    #     return position in self.floor
+    def __contains__(self, position):
+        return position in self.floor
 
-    # Associate a character to a position from a text file
+    # Associate a character to a position from a text file in lists
     def load(self, filename):
         with open(filename) as f:
-            lines = f.readlines()
-            self.width = len(lines)
-            self.height = len(lines[0])
-            for n_row, line in enumerate(lines):
-                for n_col, character in enumerate(line):
-                    if character == FLOOR_CHAR:
-                        self.floor.append(Position(n_row, n_col))
-                    # elif character == WALLS_CHAR:
-                    #     self.walls.append(Position(n_row, n_col))
-                    elif character == START_CHAR:
-                        self.start = Position(n_row, n_col)
-                        self.floor.append(Position(n_row, n_col))
+            map = f.readlines()
+            for lin, line in enumerate(map):
+                for char, character in enumerate(line):
+                    position = Position(char, lin)
+                    if character == START_CHAR:
+                        self.start = position
+                        self.floor.append(position)
                     elif character == EXIT_CHAR:
-                        self.exit = Position(n_row, n_col)
-                        self.floor.append(Position(n_row, n_col))
+                        self.exit = position
+                        self.floor.append(position)
+                    elif character == FLOOR_CHAR:
+                        self.floor.append(position)
+                    else:
+                        self.walls.append(position)
 
-    def can_move_to(self, x, y):
-        return Position(x, y) in self.maze.floor
+            self.width = char + 1
+            self.height = lin + 1
 
-    def move_hero_up(self):
-        if self.can_move_to(self.hero.x - 1, self.hero.y):
-            self.hero.up()
-            for weapon in self.weapons:
-                if self.hero == weapon:
-                    self.hero.pick_up_weapon()
+    def add_hero(self, hero):
+        self.hero = hero
+        self.hero = self.start
+        self.hero.maze = self
 
-    def move_hero_down(self):
-        if self.can_move_to(self.hero.x + 1, self.hero.y):
-            self.hero.down()
-            for weapon in self.weapons:
-                if self.hero == weapon:
-                    self.hero.pick_up_weapon()
+    # def can_move_to(self):
+    #     return Position(x, y) in self.maze.floor
 
-    def move_hero_left(self):
-        if self.can_move_to(self.hero.x, self.hero.y - 1):
-            self.hero.left()
-            for weapon in self.weapons:
-                if self.hero == weapon:
-                    self.hero.pick_up_weapon()
+    # @can_move_to
+    # def move_hero_up(self):
+    #     if self.can_move_to(self.hero.x - 1, self.hero.y):
+    #         self.hero.up()
+    #         for weapon in self.weapons:
+    #             if self.hero == weapon:
+    #                 self.hero.pick_up_weapon()
 
-    def move_hero_right(self):
-        if self.can_move_to(self.hero.x, self.hero.y + 1):
-            self.hero.right()
-            for weapon in self.weapons:
-                if self.hero == weapon:
-                    self.hero.pick_up_weapon()
+    # @can_move_to
+    # def move_hero_down(self):
+    #     if self.can_move_to(self.hero.x + 1, self.hero.y):
+    #         self.maze.down()
+    #         for weapon in self.weapons:
+    #             if self.hero == weapon:
+    #                 self.hero.pick_up_weapon()
+
+    # def move_hero_left(self):
+    #     if self.can_move_to(self.hero.x, self.hero.y - 1):
+    #         self.hero.left()
+    #         for weapon in self.weapons:
+    #             if self.hero == weapon:
+    #                 self.hero.pick_up_weapon()
+
+    # def move_hero_right(self):
+    #     if self.can_move_to(self.hero.x, self.hero.y + 1):
+    #         self.hero.right()
+    #         for weapon in self.weapons:
+    #             if self.hero == weapon:
+    #                 self.hero.pick_up_weapon()
 
     # get random positions for weapons
     def random_positions_weapons(self):
-        self.random_positions = random.sample(
+        self.weapons = random.sample(
             set(self.floor) - {self.start, self.exit}, 3)
         weapons = ["ether", "pipe", "needle"]
-        for position in random_positions():
-            self.weapons.append(Weapons(x=position.x, y=position.y, name=weapons.pop())
+        for position in weapons:
+            self.weapons.append(weapons(x=position.x, y=position.y, name=weapons.pop()))
