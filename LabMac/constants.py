ccb9a7e4dcc9e26c1df83d8ea803d99f04bd7190
Jody Etienne <etiennody@gmail.com>
2019-12-07 15:36:46 +0100

    add window+fps+images for graphics elements

--- LabMac/constants.py
+++ LabMac/constants.py
@@ -8,9 +8,30 @@ Author:
 Github:
     https://github.com/etiennody
 """
+import os
 
 # Default game constants definition
 START_CHAR = "S"
 EXIT_CHAR = "E"
 FLOOR_CHAR = "0"
 WALL_CHAR = "x"
+
+# Initialize window
+WINDOW_WIDTH = 480
+WINDOW_HEIGHT = 480
+BACKGROUND_COLOR = (255, 255, 255)
+SPRITE_SIZE = 30
+
+
+# Frame rate
+FPS = 60
+
+# Images
+MAP = os.path.join('LabMac', 'resources', 'map', 'map.txt')
+HERO = 'macgyver.png'
+GUARDIAN = 'guardian.png'
+WALL = 'wall.png'
+FLOOR = 'floor.png'
+ETHER = 'ether.png'
+NEEDLE = 'needle.png'
+PIPE = 'pipe.png'
