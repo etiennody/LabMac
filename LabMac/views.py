220250bf64dd9dc7e2e99d3009b952c5b7c96fd3
Jody Etienne <etiennody@gmail.com>
2019-12-09 17:22:11 +0100

    update MazeView

--- LabMac/views.py
+++ LabMac/views.py
@@ -10,7 +10,6 @@ Github:
 """
 import os
 import pygame
-from pygame.compat import geterror
 
 from LabMac.constants import WINDOW_WIDTH, WINDOW_HEIGHT, FPS, WALL, FLOOR, NEEDLE, ETHER, PIPE, GUARDIAN, SPRITE_SIZE
 
@@ -36,7 +35,7 @@ class LabPygame:
                 image = image.get_alpha()
         except pygame.error:
             print("Cannot load image", name)
-            raise SystemExit(str(geterror()))
+            raise SystemExit
 
         # Set transparency
         if colorkey is not None:
@@ -52,38 +51,39 @@ class MazeView(LabPygame):
         super().__init__(*args, **kwargs)
         self.maze_render = pygame.Surface(self.window_surface.get_size()).convert()
         self.maze_render.fill((25, 25, 25))
+        self.display_elements()
 
-        self.wall_img = self.load_image(WALL, -1)
-        self.wall_render = pygame.transform.scale(self.wall_img, (32, 32))
+        # self.wall_img = self.load_image(WALL, -1)
+        # self.wall_render = pygame.transform.scale(self.wall_img, (32, 32))
 
-        self.floor_img = self.load_image(FLOOR, -1)
-        self.floor_render = pygame.transform.scale(self.floor_img, (32, 32))
+        # self.floor_img = self.load_image(FLOOR, -1)
+        # self.floor_render = pygame.transform.scale(self.floor_img, (32, 32))
 
-        self.guardian_img = self.load_image(GUARDIAN, -1)
-        self.guardian_render = pygame.transform.scale(self.guardian_img, (30, 30))
+        # self.guardian_img = self.load_image(GUARDIAN, -1)
+        # self.guardian_render = pygame.transform.scale(self.guardian_img, (30, 30))
 
-        self.needle_img = self.load_image(NEEDLE, -1)
-        self.needle_render = pygame.transform.scale(self.needle_img, (30, 30))
+        # self.needle_img = self.load_image(NEEDLE, -1)
+        # self.needle_render = pygame.transform.scale(self.needle_img, (30, 30))
 
-        self.ether_img = self.load_image(ETHER, -1)
-        self.needle_render = pygame.transform.scale(self.needle_img, (30, 30))
+        # self.ether_img = self.load_image(ETHER, -1)
+        # self.needle_render = pygame.transform.scale(self.needle_img, (30, 30))
 
-        self.pipe_img = self.load_image(PIPE, -1)
-        self.pipe_render = pygame.transform.scale(self.needle_img, (30, 30))
+        # self.pipe_img = self.load_image(PIPE, -1)
+        # self.pipe_render = pygame.transform.scale(self.needle_img, (30, 30))
 
     def display_elements(self, window_surface):
-        for pos in self.walls:
-            window_surface.blit(self.wall_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
+        for pos in self.maze.walls:
+            self.window_surface.blit(self.wall_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
         for pos in self.floor:
-            window_surface.blit(self.floor_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
+            self.window_surface.blit(self.floor_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
         for weap, pos in enumerate(self.weapons):
             if weap == 0:
-                window_surface.blit(self.needle_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
+                self.window_surface.blit(self.needle_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
             elif weap == 1:
-                window_surface.blit(self.ether_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
+                self.window_surface.blit(self.ether_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
             elif weap == 2:
-                window_surface.blit(self.pipe_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
-        window_surface.blit(self.guardian_render, (self.exit.y * SPRITE_SIZE, self.exit.x * SPRITE_SIZE))
+                self.window_surface.blit(self.pipe_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
+        self.window_surface.blit(self.guardian_render, (self.exit.y * SPRITE_SIZE, self.exit.x * SPRITE_SIZE))
 
 
 class HeroView(pygame.sprite.Sprite):
