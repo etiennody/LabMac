4cbd363e0ac894b7687c9090569330b23c68d08b
Jody Etienne <etiennody@gmail.com>
2019-12-17 14:14:32 +0100

    fix display elements

--- LabMac/views.py
+++ LabMac/views.py
@@ -12,17 +12,23 @@ Github:
 import sys
 import pygame
 
-from LabMac.constants import WINDOW_WIDTH, WINDOW_HEIGHT, FPS, HERO, SPRITE_SIZE, WALL, FLOOR, BGM#, HERO, BGM, NEEDLE, ETHER, PIPE, GUARDIAN
+# from LabMac.model.position import Position
+from LabMac.constants import WINDOW_WIDTH, WINDOW_HEIGHT, FPS, HERO, SPRITE_SIZE, WALL, WALL_CHAR, FLOOR, FLOOR_CHAR, GUARDIAN, BGM#, HERO, EXIT_CHAR,#, BGM, NEEDLE, ETHER, PIPE
 
 
 class LabPygame(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        """Initialize the window display"""
         pygame.init()
-        pygame.time.Clock().tick(FPS)
-        self.window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.size = (WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.window_surface = pygame.display.set_mode(self.size)
         pygame.display.set_caption("LabMac - Help MacGyver !")
+        """Filling the background"""
+        self.background = pygame.Surface(self.window_surface.get_size())
+        self.background = self.background.convert()
+        self.background.fill((0, 0, 0))
         # """Text font"""
         # font = pygame.font.SysFont('Consolas', 15)
 
@@ -31,16 +37,16 @@ class LabPygame(pygame.sprite.Sprite):
         text_render = font.render(text, True, color)
         rect = text_render.get_rect()
         rect.left, rect.top = position
-        self.window_surface.blit(text_render, rect)
+        window_surface.blit(text_render, rect)
         return rect.right
 
     def button(self, window_surface, position, text, buttoncolor=(120, 120, 120), linecolor=(20, 20, 20), textcolor=(255, 255, 255), bwidth=200, bheight=50):
         """Create and config buttons"""
         left, top = position
-        pygame.draw.line(window_surface, linecolor, (left, top), (left+bwidth, top), 5)
-        pygame.draw.line(window_surface, linecolor, (left, top-2), (left, top+bheight), 5)
-        pygame.draw.line(window_surface, linecolor, (left, top+bheight), (left+bwidth, top+bheight), 5)
-        pygame.draw.line(window_surface, linecolor, (left+bwidth, top+bheight), (left+bwidth, top), 5)
+        pygame.draw.line(window_surface, linecolor, (left, top), (left + bwidth, top), 5)
+        pygame.draw.line(window_surface, linecolor, (left, top - 2), (left, top + bheight), 5)
+        pygame.draw.line(window_surface, linecolor, (left, top + bheight), (left + bwidth, top + bheight), 5)
+        pygame.draw.line(window_surface, linecolor, (left + bwidth, top + bheight), (left + bwidth, top), 5)
         pygame.draw.rect(window_surface, buttoncolor, (left, top, bwidth, bheight))
         font = pygame.font.SysFont('Consolas', 30)
         text_render = font.render(text, 1, textcolor)
@@ -50,7 +56,7 @@ class LabPygame(pygame.sprite.Sprite):
 
     def interface(self, mode='game_start'):
         """Create interface for start and end game with mouse"""
-        pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        # pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         if mode == 'game_start':
             clock = pygame.time.Clock()
             while True:
@@ -101,34 +107,93 @@ class LabPygame(pygame.sprite.Sprite):
 
 class MazeView(pygame.sprite.Sprite):
     def __init__(self, maze):
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__()
         self.maze = maze
-        self.floor = []
-        self.walls = []
-        self.weapons = []
-        self.hero = None
-        self.start = None
-        self.exit = None
-        self.width = None
-        self.height = None
-        self.window_surface = WINDOW_WIDTH * WINDOW_HEIGHT
+        self.window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.x = (WINDOW_WIDTH * SPRITE_SIZE)
+        self.y = (WINDOW_HEIGHT * SPRITE_SIZE)
+        # self.clock = pygame.time.Clock()
 
-    def display_elements(self, window_surface):
-        clock = pygame.time.Clock()
-        while True:
-            self.window_surface.blit(pygame.image.load(WALL), (self.maze.walls.y * SPRITE_SIZE, self.maze.walls.x * SPRITE_SIZE))
-            # self.window_surface.blit(pygame.image.load(FLOOR), (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
-            # for weap, pos in enumerate(self.weapons):
-            #     if weap == 0:
-            #         self.window_surface.blit(self.needle_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
-            #     elif weap == 1:
-            #         self.window_surface.blit(self.ether_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
-            #     elif weap == 2:
-            #         self.window_surface.blit(self.pipe_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
-            #     self.window_surface.blit(self.guardian_render, (self.exit.y * SPRITE_SIZE, self.exit.x * SPRITE_SIZE))
-            pygame.display.update()
-            clock.tick(FPS)
+        # self.maze_render = pygame.Surface(self.window_surface.get_size()).convert()
+        # self.maze_render.fill((255, 255, 255))
+        self.walls_img = pygame.image.load(WALL).convert()
+        self.walls_rect = self.walls_img.get_rect()
+        self.walls_render = pygame.transform.scale(self.walls_img, (32, 32))
 
+        self.floor_img = pygame.image.load(FLOOR).convert()
+        self.floor_rect = self.floor_img.get_rect()
+        self.floor_render = pygame.transform.scale(self.floor_img, (32, 32))
+
+        # self.guardian_img = pygame.image.load(GUARDIAN).convert_alpha()
+        # self.guardian_render = pygame.transform.scale(self.guardian_img, (30, 30))
+
+    def display_elements(self):
+        for wall in self.maze.walls:
+            # self.window_surface.blit(self.background, (wall.x, wall.y))
+            self.window_surface.blit(self.walls_render, (wall.x * SPRITE_SIZE, wall.y * SPRITE_SIZE))
+        for floor in self.maze.floor:
+            self.window_surface.blit(self.floor_render, (floor.x * SPRITE_SIZE, floor.y * SPRITE_SIZE))
+        # self.window_surface.blit(self.guardian_render, (self.exit.y * SPRITE_SIZE, self.exit.x * SPRITE_SIZE))
+        pygame.display.update()
+        # self.clock.tick(FPS)
+
+
+class HeroView(LabPygame, pygame.sprite.Sprite):
+
+    def __init__(self, maze, hero, *args, **kwargs):
+        pygame.sprite.Sprite.__init__(self)
+        super().__init__(*args, **kwargs)
+        self.hero = hero
+        self.maze = maze
+        self.hero_img = pygame.image.load(HERO)
+        self.hero_render = pygame.transform.scale(self.hero_img, (30, 30))
+        self.rect = self.hero_render.get_rect()
+        self.rect.topleft = self.maze.hero.y_pixel, self.maze.hero.x_pixel
+
+    def draw(self):
+        self.window_surface.blit(self.hero_render, self.rect)
+
+        # for guardian, pos in self.exit:
+        #     if guardian == EXIT_CHAR:
+        # self.window_surface.blit(self.guardian_render, (self.pos.x, self.pos.y))
+
+        # for lin, line in enumerate(map):
+        #     lin = 0
+        #     for char, character in enumerate(line):
+        #         char = 0
+        #     for sprite in line:
+        #         #On calcule la position réelle en pixels
+        #         x = char * SPRITE_SIZE
+        #         y = lin * SPRITE_SIZE
+        #         if sprite == 'x':          #m = Mur
+        #             self.window_surface.blit(self.walls_render, (x, y))
+        #         elif sprite == '0':        #d = Départ
+        #             self.window_surface.blit(self.floor_render, (x, y))
+        #         elif sprite == 'E':        #a = Arrivée
+        #             self.window_surface.blit(self.guardian_render, (x, y))
+        #         char += 1
+        #         lin += 1
+        # #On parcourt la liste du niveau
+        # num_ligne = 0
+        # for ligne in self.structure:
+        #     #On parcourt les listes de lignes
+        #     num_case = 0
+
+            # for floor, pos in enumerate(self.floor):
+            #     if floor == FLOOR_CHAR:
+            #         self.window_surface.blit(self.floor_render, (self.pos.x, self.pos.y))
+            # if self.exit == EXIT_CHAR:
+            #     self.window_surface.blit(self.guardian_render, (self.pos.x, self.pos.y))
+                # (self.maze.walls.position.y * SPRITE_SIZE, self.maze.walls.position.x * SPRITE_SIZE))
+                # self.window_surface.blit(pygame.image.load(FLOOR), (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
+                # for weap, pos in enumerate(self.weapons):
+                #     if weap == 0:
+                #         self.window_surface.blit(self.needle_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
+                #     elif weap == 1:
+                #         self.window_surface.blit(self.ether_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
+                #     elif weap == 2:
+                #         self.window_surface.blit(self.pipe_render, (pos.y * SPRITE_SIZE, pos.x * SPRITE_SIZE))
+                # self.window_surface.blit(self.guardian_render, (self.exit.y * SPRITE_SIZE, self.exit.x * SPRITE_SIZE))
 
 # class MazeView(LabPygame, pygame.sprite.Sprite):
 
@@ -161,18 +226,3 @@ class MazeView(pygame.sprite.Sprite):
         # self.pipe_render = pygame.transform.scale(self.needle_img, (30, 30))
 
 
-# class HeroView(LabPygame, pygame.sprite.Sprite):
-
-#     def __init__(self, maze, hero, *args, **kwargs):
-#         pygame.sprite.Sprite.__init__(self)
-#         super().__init__(*args, **kwargs)
-#         self.hero = hero
-#         self.maze = maze
-
-#         self.image = pygame.image.load("./LabMac/resources/images/macgyver.png")
-#         self.rect = self.image.get_rect()
-#         self.rect.topleft = self.maze.hero.y_pixel, self.maze.hero.x_pixel
-#         # import pdb; pdb.set_trace()
-#         self.hero = pygame.Rect(300, 100, 32, 32)
-#         self.hero_img = self.load_image(HERO, -1).convert()
-#         self.hero_render = pygame.transform.scale(self.hero_img, (32, 32))
