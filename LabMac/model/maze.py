43586c031e1951ea802eff0edfc372730cec253b
Jody Etienne <etiennody@gmail.com>
2019-12-27 19:12:42 +0100

    remove print in move hero methods

--- LabMac/model/maze.py
+++ LabMac/model/maze.py
@@ -12,10 +12,10 @@ GitHub:
 import pygame
 import random
 
-from LabMac.constants import FLOOR_CHAR, START_CHAR, EXIT_CHAR
 from LabMac.views import LabPygame
 from LabMac.model.position import Position
 from LabMac.model.items import Weapon, Hero
+from LabMac.constants import FLOOR_CHAR, START_CHAR, EXIT_CHAR
 
 
 class Maze(pygame.sprite.Sprite):
@@ -66,7 +66,6 @@ class Maze(pygame.sprite.Sprite):
             self.hero.up()
             for weapon in self.weapons:
                 if self.hero == weapon:
-                    print(f"You have {len(self.hero.inventory)} weapon(s) in your inventory")
                     self.hero.pick_up_weapon(weapon)
 
     def move_hero_down(self):
@@ -74,7 +73,6 @@ class Maze(pygame.sprite.Sprite):
             self.hero.down()
             for weapon in self.weapons:
                 if self.hero == weapon:
-                    print(f"You have {len(self.hero.inventory)} weapon(s) in your inventory")
                     self.hero.pick_up_weapon(weapon)
 
     def move_hero_left(self):
@@ -82,7 +80,6 @@ class Maze(pygame.sprite.Sprite):
             self.hero.left()
             for weapon in self.weapons:
                 if self.hero == weapon:
-                    print(f"You have {len(self.hero.inventory)} weapon(s) in your inventory")
                     self.hero.pick_up_weapon(weapon)
 
     def move_hero_right(self):
@@ -90,14 +87,7 @@ class Maze(pygame.sprite.Sprite):
             self.hero.right()
             for weapon in self.weapons:
                 if self.hero == weapon:
-                    print(f"You have {len(self.hero.inventory)} weapon(s) in your inventory")
                     self.hero.pick_up_weapon(weapon)
-
-    # def is_exit_hero(self):
-    #     return self.hero == self.exit
-
-    # def count_inventory(self):
-    #     return len(self.hero.inventory) == len(self.weapons)
 
     # All conditions to fight with the guardian at the end
     def fight_guardian(self):
@@ -108,14 +98,6 @@ class Maze(pygame.sprite.Sprite):
                 return self.labpygame.display_lose()
         else:
             return False
-
-         #    if self.hero == self.exit:
-         #    if len(self.hero.inventory) == len(self.weapons):
-         #        return "\n***** YOU WIN!! YOU HAVE FIND THE EXIT AND SEDATED THE GUARDIAN *****"
-         #     else:
-         #         return "\n*****__GAME OVER__*****"
-         # else:
-         #    return False
 
     # Get random positions for weapons
     def random_positions_weapons(self):
